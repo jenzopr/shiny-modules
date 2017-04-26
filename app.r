a958a332b36617164006b4e921768525c2d63305
Jens Preussner <jens.preussner@mpi-bn.mpg.de>
2017-04-26 17:02:25 +0200

    Initial commit

---
+++ app.r
@@ -0,0 +1,15 @@
+library(shiny)
+
+source("colorPicker.R")
+source("kmeans.R")
+
+ui <- fixedPage(
+  h2("Module example"),
+  kmeansClusInput("kmeans")
+)
+
+server <- function(input, output, session) {
+  km <- callModule(kmeansClus, "kmeans")
+}
+
+shinyApp(ui, server)
